78e0341c218088db3f997e8d9a37f17e37ed341f
Irving Bustos Ortega <bustos.ortega1996@gmail.com>
2017-10-05 00:48:48 +0000

    solicitud de biografia

---
+++ WebForm1.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="MiPrimerSitioWeb.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title></title>
+    <link rel="Stylesheet" href="css/estilos.css" />
+</head>
+<body>
+   
+            
+
+       
+    
+    <main>
+
+     <div class="row">
+   
+                    <header>
+                <h1 class="cabecera">DOCENTE</h1>
+              
+       
+                    </header>
+     </div>
+    
+         
+
+            
+        <form id="form1" runat="server"> 
+            
+         <div class="col-6">
+            <div>
+                <asp:Label ID="RFC" runat="server" Text="RFC:"></asp:Label>
+                <asp:TextBox ID="txtRFC" runat="server"></asp:TextBox>
+                
+                </div>
+            </div>
+            <div class="col-6">
+            <div>
+                <asp:Label ID="Nombre" runat="server" Text="Nombre:"></asp:Label>
+                <asp:TextBox ID="TextNombre" runat="server"></asp:TextBox>
+                </div>
+            </div>
+            <div class="col-6">
+            <div>
+                <asp:Label ID="Apellido_pat" runat="server" Text="Apellido_pat:"></asp:Label>
+                <asp:TextBox ID="TextApellido_pat" runat="server"></asp:TextBox>
+                </div>
+            </div>
+            <div class="col-6">
+            <div>
+                <asp:Label ID="Apellido_mat" runat="server" Text="Apellido_mat:"></asp:Label>
+                <asp:TextBox ID="TextApellido_mat" runat="server"></asp:TextBox>
+                </div>
+            </div>
+            <div class="col-6">
+            <div>
+            <asp:Button ID="Enviar" runat="server" Text="Enviar" />
+            </div>    
+            </div>
+        </form>
+           
+  
+        
+
+
+    </main>
+</body>
+</html>
---
+++ WebForm1.aspx.designer.cs
@@ -0,0 +1,105 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace MiPrimerSitioWeb {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control RFC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label RFC;
+        
+        /// <summary>
+        /// Control txtRFC.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtRFC;
+        
+        /// <summary>
+        /// Control Nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Nombre;
+        
+        /// <summary>
+        /// Control TextNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextNombre;
+        
+        /// <summary>
+        /// Control Apellido_pat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Apellido_pat;
+        
+        /// <summary>
+        /// Control TextApellido_pat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextApellido_pat;
+        
+        /// <summary>
+        /// Control Apellido_mat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Apellido_mat;
+        
+        /// <summary>
+        /// Control TextApellido_mat.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextApellido_mat;
+        
+        /// <summary>
+        /// Control Enviar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Enviar;
+    }
+}
